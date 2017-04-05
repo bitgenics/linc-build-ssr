3cea7eac6c863818277a09ab4fbe7c848f67a472
Erwin van der Koogh <erwin@bitgenics.io>
2017-04-05 17:27:53 +1000

    Now take profile from package.json

--- linc-build-ssr/src/index.js
+++ linc-build-ssr/src/index.js
@@ -1,7 +1,8 @@
 const path = require('path');
 
 const build = (config, packageJson, callback) => {
-	const profile = 'generic-react-redux-routerv3';
+	const profile = (packageJson.linc && packageJson.linc.buildProfile) || 'generic-react-redux-routerv3';
+	console.log(`Using build profile: ${profile}`);
 	const builder = require(path.resolve(process.cwd(), `node_modules/@bitgenics/linc-profile-${profile}`));
 	builder(callback);
 }
