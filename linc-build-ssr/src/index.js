75d377c8d6cd7adc63b44b4ff0f44e91a4bad869
Erwin van der Koogh <erwin@bitgenics.io>
2017-04-19 14:14:57 +1000

    Use full module name as profile and copy over error pages

--- linc-build-ssr/src/index.js
+++ linc-build-ssr/src/index.js
@@ -1,9 +1,16 @@
 const path = require('path');
+const fse = require('fs-extra');
 
 const build = (config, packageJson, callback) => {
-	const profile = (packageJson.linc && packageJson.linc.buildProfile) || 'generic-react-redux-routerv3';
+	const lincConfig = packageJson.linc || {};
+	const profile = lincConfig.buildProfile || '@bitgenics/linc-profile-generic-react-redux-routerv3';
+	const errorHtmls = lincConfig.errorDir || 'errors';
+	const errorSrc = path.resolve(process.cwd(), errorHtmls);
+	const errorDest = path.resolve(process.cwd(), 'dist', 'static', '_errors');
+	fse.ensureDir(errorDest);
+	fse.copySync(errorSrc, errorDest);
 	console.log(`Using build profile: ${profile}`);
-	const builder = require(path.resolve(process.cwd(), `node_modules/@bitgenics/linc-profile-${profile}`));
+	const builder = require(path.resolve(process.cwd(), `node_modules/${profile}`));
 	builder(callback);
 }
 
