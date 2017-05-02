66d49d55579d311ce95828930328d078e505a7a1
Erwin van der Koogh <erwin@bitgenics.io>
2017-05-02 10:44:41 +0800

    Use the public repository by default

--- linc-build-ssr/src/index.js
+++ linc-build-ssr/src/index.js
@@ -3,7 +3,7 @@ const fse = require('fs-extra');
 
 const build = (config, packageJson, callback) => {
 	const lincConfig = packageJson.linc || {};
-	const profile = lincConfig.buildProfile || '@bitgenics/linc-profile-generic-react-redux-routerv3';
+	const profile = lincConfig.buildProfile || 'linc-profile-generic-react-redux-routerv3';
 	const errorHtmls = lincConfig.errorDir || 'errors';
 	const errorSrc = path.resolve(process.cwd(), errorHtmls);
 	const errorDest = path.resolve(process.cwd(), 'dist', 'static', '_errors');
