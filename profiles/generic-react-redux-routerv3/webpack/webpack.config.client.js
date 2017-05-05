d33e62c449b58c7e5b572a6bcc2385c87698a20b
Erwin van der Koogh <erwin@bitgenics.io>
2017-05-05 14:41:38 +1000

    Massive rework for web pack configs. Support for custom Babel configurations, and Stylus support

--- profiles/generic-react-redux-routerv3/webpack/webpack.config.client.js
+++ profiles/generic-react-redux-routerv3/webpack/webpack.config.client.js
@@ -1,10 +1,13 @@
 const path = require('path');
+const fs = require('fs');
 const webpack = require('webpack');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
-const InterpolateHtmlPlugin = require('react-dev-utils/InterpolateHtmlPlugin');
 const ManifestPlugin = require('webpack-manifest-plugin');
 
 process.env.NODE_ENV = 'production';
+
+const LINC_DIR = path.resolve(__dirname, '..');
+const PROJECT_DIR = process.cwd();
 
 function ensureSlash(path, needsSlash) {
   var hasSlash = path.endsWith('/');
@@ -17,9 +20,12 @@ function ensureSlash(path, needsSlash) {
   }
 }
 
-const packageJson = require(path.resolve(process.cwd(), 'package.json'));
+const packageJson = require(path.resolve(PROJECT_DIR, 'package.json'));
 const lincConfig = packageJson.linc || {};
-const srcDir = lincConfig.src || 'src';
+const srcDir = lincConfig.sourceDir || 'src';
+
+const deps = Object.keys(packageJson.dependencies).concat(Object.keys(packageJson.devDependencies));
+const hasLess = deps.includes('less');
 
 // We use "homepage" field to infer "public path" at which the app is served.
 // Webpack needs to know it to put the right <script> hrefs into HTML even in
@@ -36,22 +42,85 @@ var publicPath = ensureSlash(homepagePathname, true);
 // Omit trailing slash as %PUBLIC_PATH%/xyz looks better than %PUBLIC_PATH%xyz.
 var publicUrl = ensureSlash(homepagePathname, false);
 
+const url_loader_config = {
+  exclude: [
+    /\.html$/,
+    /\.(js|jsx)$/,
+    /\.css$/,
+    /\.json$/,
+    /\.svg$/,
+    /\.woff$/,
+    /\.woff2$/,
+    /\.eot$/,
+    /\.ttf$/,
+  ],
+  loader: 'url-loader',
+  query: {
+    limit: 10000,
+    name: '_assets/media/[name].[hash:8].[ext]'
+  }
+}
+
+const extractPlugin = ExtractTextPlugin.extract({
+  fallback: { loader: 'style-loader', options: { sourceMap: true } },
+  use:[{ 
+    loader: 'css-loader', 
+    options: { sourceMap: true }
+  }]
+})
+
+const css_loader = {
+  test: /\.(css)$/,
+  loader: extractPlugin
+}
+
+const babel_config = {
+  loader: {
+    test: /\.js$/,
+    loader: 'babel-loader',
+    exclude: /node_modules(?!\/linc-profile-generic-react-redux-routerv3\/client\.js$)/,
+    query: {}
+  },
+  query: {
+    presets: {},
+    plugins: {}
+  }
+}
+
+try {
+  const contents = fs.readFileSync(path.resolve(PROJECT_DIR, '.babelrc'));
+  const config = JSON.parse(contents);
+  if(config.presets) {
+    config.presets.forEach((elem) => {if(elem.indexOf('react') < 0) babel_config.query.presets[elem] = ''});   
+  }
+  if(config.plugins) {
+    config.plugins.forEach((elem) => {if(elem.indexOf('react') < 0) babel_config.query.plugins[elem] = ''});
+  }
+} catch (e) {
+  //ignore
+}
+
+babel_config.loader.query.presets = Object.keys(babel_config.query.presets).map((elem) => path.resolve(PROJECT_DIR, 'node_modules', `babel-preset-${elem}`));
+babel_config.loader.query.plugins = Object.keys(babel_config.query.plugins).map((elem) => path.resolve(PROJECT_DIR, 'node_modules', `babel-plugin-${elem}`));
+
+babel_config.loader.query.presets.push(path.resolve(LINC_DIR, 'node_modules', `babel-preset-react-app`))
+
 module.exports = {
   entry: {
-    'main': [path.resolve(__dirname, '../client.js')]
+    'main': [path.resolve(LINC_DIR, 'client.js')]
   },
 
   resolve: {
     alias: {
-      'linc-config-js': path.resolve(process.cwd(), srcDir, 'linc.config.js')
+      'linc-config-js': path.resolve(PROJECT_DIR, srcDir, 'linc.config.js')
     },
 
-    modules: ["node_modules", path.resolve(process.cwd(), "node_modules")],
+    modules: ["node_modules", path.resolve(PROJECT_DIR, "node_modules")],
     extensions: [".js", ".json", ".ts", ".tsx"]
   },
   output: {
     // The build folder.
-    path: path.resolve(process.cwd(), 'dist', 'static'),
+    path: path.resolve(PROJECT_DIR, 'dist', 'static'),
     // Generated JS file names (with nested folders).
     // There will be one main bundle, and one file per asynchronous chunk.
     // We don't currently advertise code splitting but Webpack supports it.
@@ -63,25 +132,7 @@ module.exports = {
 
   module: {
     rules: [
-      {
-        exclude: [
-          /\.html$/,
-          /\.(js|jsx)$/,
-          /\.css$/,
-          /\.json$/,
-          /\.svg$/,
-          /\.woff$/,
-          /\.woff2$/,
-          /\.eot$/,
-          /\.ttf$/,
-          /\.(ts|tsx)$/
-        ],
-        loader: 'url-loader',
-        query: {
-          limit: 10000,
-          name: '_assets/media/[name].[hash:8].[ext]'
-        }
-      },
+      url_loader_config,
       {
         test: /\.svg$/,
         loader: 'svg-url-loader',
@@ -91,27 +142,8 @@ module.exports = {
         }
       },
       { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
-      { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules(?!\/linc-profile-generic-react-redux-routerv3\/client\.js$)/,
-        query: {
-          presets: ['react-app']
-        }
-      },
-      {
-        test: /\.(css)$/,
-        loader: ExtractTextPlugin.extract({
-          fallback: { loader: 'style-loader', options: { sourceMap: true } },
-          use:[
-                { 
-                  loader: 'css-loader', 
-                  options: { sourceMap: true }
-                }
-              ]
-        })
-      },
+      babel_config.loader,
+      css_loader,
       {
         test: /\.(woff|woff2|eot|ttf)$/,
         loader: 'file-loader',
@@ -167,3 +199,15 @@ module.exports = {
   // 'eval' | 'cheap-eval-source-map' | 'cheap-module-eval-source-map' | 'eval-source-map'
   devtool: 'source-map'
 };
+
+if(deps.includes('typescript')) {
+  url_loader_config.exclude.push(/\.(ts|tsx)$/);
+  module.exports.module.rules.push({ test: /\.tsx?$/, loader: "awesome-typescript-loader" })
+}
+
+if(deps.includes('stylus')) {
+  url_loader_config.exclude.push(/\.(styl)$/);
+  css_loader.test = /\.(css|styl)$/
+  extractPlugin.push({loader: 'stylus-loader',});
+  console.log(extractPlugin);
+}
--- profiles/generic-react-redux-routerv3/webpack/webpack.config.server.js
+++ profiles/generic-react-redux-routerv3/webpack/webpack.config.server.js
@@ -1,29 +1,82 @@
 const path = require('path');
+const fs = require('fs');
 const webpack = require('webpack');
 
 process.env.NODE_ENV = 'production';
+const LINC_DIR = path.resolve(__dirname, '..');
+const PROJECT_DIR = process.cwd();
 
-const packageJson = require(path.resolve(process.cwd(), 'package.json'));
+const packageJson = require(path.resolve(PROJECT_DIR, 'package.json'));
 const lincConfig = packageJson.linc || {};
-const srcDir = lincConfig.src || 'src';
+const srcDir = lincConfig.sourceDir || 'src';
+
+const url_loader_config = {
+  exclude: [
+    /\.html$/,
+    /\.(js|jsx)$/,
+    /\.css$/,
+    /\.json$/,
+    /\.svg$/,
+    /\.woff$/,
+    /\.woff2$/,
+    /\.eot$/,
+    /\.ttf$/,
+  ],
+  loader: 'url-loader',
+  query: {
+    limit: 10000,
+    name: '_assets/media/[name].[hash:8].[ext]'
+  }
+}
+
+const babel_config = {
+  loader: {
+    test: /\.js$/,
+    loader: 'babel-loader',
+    exclude: /node_modules(?!\/linc-profile-generic-react-redux-routerv3\/render\.js$)/,
+    query: {}
+  },
+  query: {
+    presets: {},
+    plugins: {}
+  }
+}
+
+try {
+  const contents = fs.readFileSync(path.resolve(PROJECT_DIR, '.babelrc'));
+  const config = JSON.parse(contents);
+  if(config.presets) {
+    config.presets.forEach((elem) => {if(elem.indexOf('react') < 0) babel_config.query.presets[elem] = ''});   
+  }
+  if(config.plugins) {
+    config.plugins.forEach((elem) => {if(elem.indexOf('react') < 0) babel_config.query.plugins[elem] = ''});
+  }
+} catch (e) {
+  //ignore
+}
+
+babel_config.loader.query.presets = Object.keys(babel_config.query.presets).map((elem) => path.resolve(PROJECT_DIR, 'node_modules', `babel-preset-${elem}`));
+babel_config.loader.query.plugins = Object.keys(babel_config.query.plugins).map((elem) => path.resolve(PROJECT_DIR, 'node_modules', `babel-plugin-${elem}`));
+
+babel_config.loader.query.presets.push(path.resolve(LINC_DIR, 'node_modules', `babel-preset-react-app`))
 
 module.exports = {
   entry: {
-    'server-render': [path.resolve(__dirname, '../render.js')]
+    'server-render': [path.resolve(LINC_DIR, 'render.js')]
   },
   target: 'node',
   resolve: {
     alias: {
-      'linc-config-js': path.resolve(process.cwd(), srcDir, 'linc.config.js'),
-      'linc-server-config-js': path.resolve(process.cwd(), srcDir, 'linc.server.config.js'),
-      'asset-manifest': path.resolve(process.cwd(), 'dist', 'lib','asset-manifest.json')
+      'linc-config-js': path.resolve(PROJECT_DIR, srcDir, 'linc.config.js'),
+      'linc-server-config-js': path.resolve(PROJECT_DIR, srcDir, 'linc.server.config.js'),
+      'asset-manifest': path.resolve(PROJECT_DIR, 'dist', 'lib','asset-manifest.json')
     },
     extensions: [".js", ".json", ".ts", ".tsx"],
-    modules: ["node_modules", path.resolve(process.cwd(), "node_modules")],
+    modules: ["node_modules", path.resolve(PROJECT_DIR, "node_modules")],
   },
 
   output: {
-    path: path.resolve(process.cwd(), 'dist', 'static'),
+    path: path.resolve(PROJECT_DIR, 'dist', 'static'),
     filename: '../lib/[name].js',
     library: 'server',
     libraryTarget: 'commonjs2'
@@ -42,7 +95,6 @@ module.exports = {
           /\.woff2$/,
           /\.eot$/,
           /\.ttf$/,
-          /\.(ts|tsx)$/
         ],
         loader: 'url-loader',
         query: {
@@ -59,15 +111,7 @@ module.exports = {
         }
       },
       { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
-      { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules(?!\/linc-profile-generic-react-redux-routerv3\/render\.js$)/,
-        query: {
-          presets: ['react-app']
-        }
-      },
+      babel_config.loader,
       {
         include: /\.(css)$/,
         loader: 'ignore-loader'
@@ -94,3 +138,9 @@ module.exports = {
   // 'eval' | 'cheap-eval-source-map' | 'cheap-module-eval-source-map' | 'eval-source-map'
   devtool: 'source-map'
 };
+
+const deps = Object.keys(packageJson.dependencies).concat(Object.keys(packageJson.devDependencies));
+if(deps.includes('typescript')) {
+  url_loader_config.exclude.push(/\.(ts|tsx)$/);
+  module.exports.module.rules.push({ test: /\.tsx?$/, loader: "awesome-typescript-loader" })
+}
