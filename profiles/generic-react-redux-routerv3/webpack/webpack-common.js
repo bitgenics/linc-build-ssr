f543895ee57e431c009b9693d17cb49f4976543c
Erwin van der Koogh <erwin@bitgenics.io>
2017-08-07 22:54:29 +1000

    Release for Fix TypeScript & Babel problem.

--- profiles/generic-react-redux-routerv3/webpack/webpack-common.js
+++ profiles/generic-react-redux-routerv3/webpack/webpack-common.js
@@ -14,46 +14,6 @@ const srcDir = path.resolve(PROJECT_DIR, src);
 
 const deps = Object.keys(packageJson.dependencies).concat(Object.keys(packageJson.devDependencies));
 
-const babel_config = {
-  test: /\.js$/,
-  loader: 'babel-loader',
-  exclude: /node_modules(?!\/linc-profile-generic-react-redux-routerv3\/(render|client)\.js$)/,
-  options: {}
-}
-
-const makeBabelComponentAbsolute = (type, config) => {
-  const name = Array.isArray(config) ? config[0] : config;
-  const babel_name = name.indexOf('babel') === 0 ? name : `babel-${type}-${name}`;
-  const p = path.resolve(PROJECT_DIR, 'node_modules', babel_name);
-  if(Array.isArray(config)) {
-    config[0] = p;
-    return config
-  } else {
-    return p;
-  }
-}
-
-try {
-  const contents = fs.readFileSync(path.resolve(PROJECT_DIR, '.babelrc'));
-  const config = JSON.parse(contents);
-  config.presets = config.presets || [];
-  config.plugins = config.plugins || [];
-  config.presets = config.presets.map((elem) => makeBabelComponentAbsolute('preset', elem));
-  config.plugins = config.plugins.map((elem) => makeBabelComponentAbsolute('plugin', elem));
-  babel_config.options = config;
-} catch (e) {
-  //ignore
-}
-
-babel_config.options.presets = babel_config.options.presets || [];
-const linc_babel_react_app = path.resolve(LINC_DIR, 'node_modules', 'babel-preset-react-app');
-if(fs.existsSync(linc_babel_react_app) ) {
-  babel_config.options.presets.push(path.resolve(linc_babel_react_app));  
-} else {
-  babel_config.options.presets.push(path.resolve(PROJECT_DIR, 'node_modules', 'babel-preset-react-app'));
-}
-
-
 const env = lincConfig.build_env || {};
 const defineEnv = {};
 Object.keys(env).forEach((key) => defineEnv[key] = JSON.stringify(env[key]));
