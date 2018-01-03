29243ed0b3030d0b54f06b6cf612300c56e41801
Sander Huijsen <sander.huijsen@gmail.com>
2018-01-03 17:47:12 +0800

    Call builder.build() if present, otherwise revert to calling builder().

--- linc-build-ssr/src/index.js
+++ linc-build-ssr/src/index.js
@@ -1,5 +1,5 @@
-const path = require('path');
-const fse = require('fs-extra');
+const path = require('path')
+const fse = require('fs-extra')
 
 /**
  * Build a site based on the profile provided in package.json.
@@ -8,18 +8,26 @@ const fse = require('fs-extra');
  * @param callback
  */
 const build = (config, packageJson, callback) => {
-	const lincConfig = packageJson.linc || {};
-	const profile = lincConfig.buildProfile || 'linc-profile-generic-react-redux-routerv3';
-	const errorHtmls = lincConfig.errorDir || 'errors';
-	const errorSrc = path.resolve(process.cwd(), errorHtmls);
-	if (fse.pathExistsSync(errorSrc) ){
-		const errorDest = path.resolve(process.cwd(), 'dist', 'static', '_errors');
-		fse.ensureDir(errorDest);
-		fse.copySync(errorSrc, errorDest);
-	}
-	console.log(`Using build profile: ${profile}`);
-	const builder = require(path.resolve(process.cwd(), `node_modules/${profile}`));
-	builder(callback);
-};
+  const lincConfig = packageJson.linc || {}
+  const profile =
+    lincConfig.buildProfile || 'linc-profile-generic-react-redux-routerv3'
+  const errorHtmls = lincConfig.errorDir || 'errors'
+  const errorSrc = path.resolve(process.cwd(), errorHtmls)
+  if (fse.pathExistsSync(errorSrc)) {
+    const errorDest = path.resolve(process.cwd(), 'dist', 'static', '_errors')
+    fse.ensureDir(errorDest)
+    fse.copySync(errorSrc, errorDest)
+  }
+  console.log(`Using build profile: ${profile}`)
+  const builder = require(path.resolve(
+    process.cwd(),
+    `node_modules/${profile}`
+  ))
+  if (builder.build && typeof builder.build === 'function') {
+    return builder.build(callback)
+  }
 
-module.exports = build;
+  return builder(callback)
+}
+
+module.exports = build
