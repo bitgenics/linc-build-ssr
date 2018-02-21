e75e6113a2ee7a98aa16f95b4423880024a34666
Sander Huijsen <sander.huijsen@gmail.com>
2018-02-21 12:50:02 +0800

    Renamed promiseCounter config to config-redux.

--- profiles/react/src/strategy.js
+++ profiles/react/src/strategy.js
@@ -24,7 +24,7 @@ const pickRouter = deps => {
 
 const pickStatePromise = deps => {
   if (deps['redux']) {
-    return 'promiseCounter'
+    return 'config-redux'
   } else {
     return 'config-state'
   }
