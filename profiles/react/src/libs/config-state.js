5973b3bad4ad3f9bee70f2d0e76b0c911c54b77b
Sander Huijsen <sander.huijsen@gmail.com>
2018-02-27 17:36:01 +0800

    Changed the way the state promise function is called (the one in the config file).

--- profiles/react/src/libs/config-state.js
+++ profiles/react/src/libs/config-state.js
@@ -1,20 +1,20 @@
-const configMethod = (req, config, route, routeComponent) => {
-  return new Promise((resolve, reject) => {
-    config.getStatePromise(req, config, route, routeComponent, (err, state) => {
-      if (err) {
-        return reject(err)
-      }
-      resolve(state)
-    })
-  })
-}
-
 const getStatePromiseFn = (req, config, route, routeComponent) => {
-  if (config.state && config.state.getStatePromise) {
-    return configMethod
-  } else {
-    Promise.resolve({})
+  if (!config.state || config.state.getStatePromise) {
+    return Promise.resolve({})
   }
+
+  return new Promise(async (resolve, reject) => {
+    try {
+      const state = await config.state.getStatePromise(
+        req,
+        route,
+        routeComponent
+      )
+      return resolve(state)
+    } catch (e) {
+      return reject(e)
+    }
+  })
 }
 
 const getStatePromise = {
